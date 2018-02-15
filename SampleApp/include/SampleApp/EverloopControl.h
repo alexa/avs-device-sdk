a6ca25a4725cc2dca59a0131558e6fd7e240c511
Yoel Ramos Castillo <yoelrc88@gmail.com>
2018-02-15 00:15:36 +0000

    adding new class for everloop control

---
+++ SampleApp/include/SampleApp/EverloopControl.h
@@ -0,0 +1,46 @@
+/*
+ * Copyright 2017-2018 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License").
+ * You may not use this file except in compliance with the License.
+ * A copy of the License is located at
+ *
+ *     http://aws.amazon.com/apache2.0/
+ *
+ * or in the "license" file accompanying this file. This file is distributed
+ * on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+
+#ifndef ALEXA_CLIENT_SDK_SAMPLEAPP_INCLUDE_SAMPLEAPP_EVERLOOPCONTROL_H_
+#define ALEXA_CLIENT_SDK_SAMPLEAPP_INCLUDE_SAMPLEAPP_EVERLOOPCONTROL_H_
+
+#include <AVSCommon/SDKInterfaces/DialogUXStateObserverInterface.h>
+#include <AVSCommon/Utils/Threading/Executor.h>
+
+namespace alexaClientSDK {
+namespace sampleApp {
+
+/**
+ * This class manages the states that the user will see when interacting with the Sample Application. For now, it simply
+ * prints states to the screen.
+ */
+class EverloopControl
+        : public avsCommon::sdkInterfaces::DialogUXStateObserverInterface{
+public:
+    void onDialogUXStateChanged(DialogUXState state) override;
+
+
+private:
+    /// The current dialog UX state of the SDK
+    DialogUXState m_dialogState;
+
+    /// An internal executor that performs execution of callable objects passed to it sequentially but asynchronously.
+    avsCommon::utils::threading::Executor m_executor;
+};
+
+}  // namespace sampleApp
+}  // namespace alexaClientSDK
+
+#endif  // ALEXA_CLIENT_SDK_SAMPLEAPP_INCLUDE_SAMPLEAPP_EVERLOOPCONTROL_H_
---
+++ SampleApp/src/EverloopControl.cpp
@@ -0,0 +1,39 @@
+/*
+ * Copyright 2017-2018 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License").
+ * You may not use this file except in compliance with the License.
+ * A copy of the License is located at
+ *
+ *     http://aws.amazon.com/apache2.0/
+ *
+ * or in the "license" file accompanying this file. This file is distributed
+ * on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+
+#include <sstream>
+
+#include "SampleApp/EverloopControl.h"
+
+#include <AVSCommon/SDKInterfaces/DialogUXStateObserverInterface.h>
+#include "AVSCommon/Utils/SDKVersion.h"
+
+#include "SampleApp/ConsolePrinter.h"
+
+namespace alexaClientSDK {
+namespace sampleApp {
+
+using namespace avsCommon::sdkInterfaces;
+
+static const std::string VERSION = avsCommon::utils::sdkVersion::getCurrentVersion();
+
+void EverloopControl::onDialogUXStateChanged(DialogUXState state) {
+    m_executor.submit([this, state]() {
+        printf("EverloopControl: new state :",stateToString(state));
+    });
+}
+
+}  // namespace sampleApp
+}  // namespace alexaClientSDK
