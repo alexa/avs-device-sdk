74a58a870b37841fa32a096605f5cc2b4e2dc760
Dan F <danielf@xmos.com>
2018-06-22 13:56:51 +0000

    Adjusted setup script. Removed unnecessary scripts.

--- tools/Install/setup.sh
+++ tools/Install/setup.sh
@@ -1,35 +1,41 @@
 #!/bin/bash
 
-# 
+#
 # Copyright 2018 Amazon.com, Inc. or its affiliates. All Rights Reserved.
-# 
+#
 # Licensed under the Apache License, Version 2.0 (the "License").
 # You may not use this file except in compliance with the License.
 # A copy of the License is located at
-# 
+#
 #  http://aws.amazon.com/apache2.0
-# 
+#
 # or in the "license" file accompanying this file. This file is distributed
 # on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
 # express or implied. See the License for the specific language governing
 # permissions and limitations under the License.
-# 
+#
 
+#
+# Modified by XMOS Ltd
+# https://github.com/xmos/avs-device-sdk
+#
 
-LOCALE=${LOCALE:-'en-US'}
 
 PORT_AUDIO_FILE="pa_stable_v190600_20161030.tgz"
 PORT_AUDIO_DOWNLOAD_URL="http://www.portaudio.com/archives/$PORT_AUDIO_FILE"
 
-TEST_MODEL_DOWNLOAD="https://github.com/Sensory/alexa-rpi/blob/master/models/spot-alexa-rpi-31000.snsr"
 
 BUILD_TESTS=${BUILD_TESTS:-'true'}
 
-CURRENT_DIR="$(pwd)"
-INSTALL_BASE=${INSTALL_BASE:-"$CURRENT_DIR"}
+pushd "$( dirname "${BASH_SOURCE[0]}" )" > /dev/null
+CURRENT_DIR="$( pwd )"
+THIS_SCRIPT="$CURRENT_DIR/setup.sh"
+popd > /dev/null
+
+INSTALL_BASE=${INSTALL_BASE:-"$HOME/sdk-folder"}
 SOURCE_FOLDER=${SDK_LOC:-''}
 THIRD_PARTY_FOLDER=${THIRD_PARTY_LOC:-'third-party'}
-BUILD_FOLDER=${BUILD_FOLDER:-'build'}
+BUILD_FOLDER=${BUILD_FOLDER:-'sdk-build'}
 SOUNDS_FOLDER=${SOUNDS_FOLDER:-'sounds'}
 DB_FOLDER=${DB_FOLDER:-'db'}
 
@@ -42,37 +48,12 @@ CONFIG_DB_PATH="$DB_PATH"
 UNIT_TEST_MODEL_PATH="$INSTALL_BASE/avs-device-sdk/KWD/inputs/SensoryModels/"
 UNIT_TEST_MODEL="$THIRD_PARTY_PATH/alexa-rpi/models/spot-alexa-rpi-31000.snsr"
 CONFIG_FILE="$BUILD_PATH/Integration/AlexaClientSDKConfig.json"
-START_AUTH_SCRIPT="$INSTALL_BASE/startauth.sh"
-TEST_SCRIPT="$INSTALL_BASE/test.sh"
+START_AUTH_SCRIPT="$INSTALL_BASE/avs_auth.sh"
+TEST_SCRIPT="$INSTALL_BASE/avs_test.sh"
 LIB_SUFFIX="a"
 
-get_platform() {
-  uname_str=`uname -a`
-
-  if [[ "$uname_str" ==  "Linux raspberrypi"* ]]
-  then
-    result="pi"
-  elif [[ "$uname_str" ==  "MINGW64"* ]]
-  then
-    result="mingw64"
-  else
-    result=""
-  fi
-}
-
-get_platform
-PLATFORM=$result
-
-if [ "$PLATFORM" == "pi" ]
-then
-  source pi.sh
-elif [ "$PLATFORM" == "mingw64" ]
-then
-  source mingw.sh
-else
-  echo "The installation script doesn't support current system. (System: $(uname -a))"
-  exit 1
-fi
+SENSORY_MODEL_HASH=5d811d92fb89043f4a4a7b7d0d26d7c3c83899b0
+ALIASES=$HOME/.bash_aliases
 
 echo "################################################################################"
 echo "################################################################################"
@@ -114,45 +95,94 @@ else
   exit 1
 fi
 
-if [ $# -eq 0 ]
-then
-  echo  'bash setup.sh <config-file>'
-  echo  'the config file must contain the following:'
-  echo  '   CLIENT_ID=<OAuth client ID>'
-  echo  '   CLIENT_SECRET=<OAuth client secret>'  
-  echo  '   PRODUCT_NAME=<your product name for device>'
-  echo  '   DEVICE_SERIAL_NUMBER=<your device serial number>'
+config_userinput(){
+    unset CLIENT_ID
+    unset CLIENT_SECRET
+    unset PRODUCT_ID
+    unset DEVICE_SERIAL_NUMBER
+    unset LOCALE
 
-  exit 1
-fi
+    while [[ -z $CLIENT_ID ]] ; do
+        echo "Enter your Client ID:"
+        read CLIENT_ID
+        if [[ ! "$CLIENT_ID" =~ amzn1\.application-oa2-client\.[0-9a-z]{32} ]]
+        then
+           echo 'Client ID is invalid!'
+           unset CLIENT_ID
+        fi
+    done
+    while [[ -z $CLIENT_SECRET ]] ; do
+        echo "Enter your Client Secret:"
+        read CLIENT_SECRET
+        if [[ ! "$CLIENT_SECRET" =~ [0-9a-f]{64} ]]
+        then
+           echo 'Client Secret is invalid!'
+           unset CLIENT_SECRET
+        fi
+    done
+    while [[ -z $PRODUCT_ID ]] ; do
+        echo "Enter your Product ID:"
+        read PRODUCT_ID
+        if [[ ! "$PRODUCT_ID" =~ [0-9a-zA-Z_]+ ]]
+        then
+           echo 'Product ID is invalid!'
+           unset PRODUCT_ID
+        fi
+    done
+    while [[ -z $DEVICE_SERIAL_NUMBER ]] ; do
+        echo "Enter your Device Serial Number (this can be any number):"
+        read DEVICE_SERIAL_NUMBER
+        if [[ ! "$DEVICE_SERIAL_NUMBER" =~ [0-9a-zA-Z_]+ ]]
+        then
+           echo 'device serial number is invalid!'
+           unset DEVICE_SERIAL_NUMBER
+        fi
+    done
+    while [[ ( "$LOCALE" != "en-US" ) && \
+             ( "$LOCALE" != "en-GB" ) && \
+             ( "$LOCALE" != "de-DE" ) ]] ; do
+        echo "Enter your desired locale (valid values are en-US, en-GB, de-DE):"
+        read LOCALE
+    done
+}
 
-source $1
+config_autostart(){
+    AUTOSTART_SESSION="avsrun"
+    AUTOSTART=$HOME/.config/lxsession/LXDE-pi/autostart
+    STARTUP_SCRIPT=$CURRENT_DIR/.avsrun-startup.sh
+
+    cat << EOF > "$STARTUP_SCRIPT"
+    #!/bin/bash
+    $BUILD_PATH/SampleApp/src/SampleApp $CONFIG_FILE $THIRD_PARTY_PATH/alexa-rpi/models
+    \$SHELL
+EOF
+    chmod a+rx $STARTUP_SCRIPT
+
+    while true; do
+        read -p "Automatically run AVS SDK at startup (y/n)? " ANSWER
+        case ${ANSWER} in
+            n|N|no|NO )
+                grep $AUTOSTART_SESSION $AUTOSTART > /dev/null 2>&1
+                if [ $? == 0 ]; then
+                    # Remove startup script from autostart file
+                    sed -i '/'"$AUTOSTART_SESSION"'/d' $AUTOSTART
+                fi
+                break;;
+            y|Y|yes|YES )
+                grep $AUTOSTART_SESSION $AUTOSTART > /dev/null 2>&1
+                if [ $? != 0 ]; then
+                    # Append startup script if not already in autostart file
+                    echo "@lxterminal -t $AUTOSTART_SESSION --geometry=150x50 -e $STARTUP_SCRIPT" >> $AUTOSTART
+                fi
+                break;;
+        esac
+    done
+}
+
+config_userinput
+config_autostart
 
 set -e
-
-if [[ ! "$CLIENT_ID" =~ amzn1\.application-oa2-client\.[0-9a-z]{32} ]]
-then
-   echo 'client ID is invalid!'
-   exit 1
-fi
-
-if [[ ! "$CLIENT_SECRET" =~ [0-9a-f]{64} ]]
-then 
-   echo 'client secret is invalid!'
-   exit 1
-fi
-
-if [[ ! "$PRODUCT_ID" =~ [0-9a-zA-Z_]+ ]]
-then 
-   echo 'product ID is invalid!'
-   exit 1
-fi
-
-if [[ ! "$DEVICE_SERIAL_NUMBER" =~ [0-9a-zA-Z_]+ ]]
-then 
-   echo 'device serial number is invalid!'
-   exit 1
-fi
 
 if [ ! -d "$BUILD_PATH" ]
 then
@@ -161,7 +191,9 @@ then
     echo "==============> INSTALLING REQUIRED TOOLS AND PACKAGE ============"
     echo
 
-    install_dependencies
+    sudo apt-get update
+    sudo apt-get -y install git gcc cmake build-essential libsqlite3-dev libcurl4-openssl-dev libfaad-dev libsoup2.4-dev libgcrypt20-dev libgstreamer-plugins-bad1.0-dev gstreamer1.0-plugins-good libasound2-dev sox gedit vim python3-pip
+    pip install flask commentjson
 
     # create / paths
     echo
@@ -187,15 +219,25 @@ then
     ./configure --without-jack
     make
 
-    run_os_specifics
+    #get sensory and build
+    echo
+    echo "==============> CLONING AND BUILDING SENSORY =============="
+    echo
 
-    #get sdk 
+    cd $THIRD_PARTY_PATH
+    git clone git://github.com/Sensory/alexa-rpi.git
+    pushd alexa-rpi > /dev/null
+    git checkout $SENSORY_MODEL_HASH -- models/spot-alexa-rpi-31000.snsr
+    popd > /dev/null
+    bash ./alexa-rpi/bin/license.sh
+
+    #get sdk
     echo
     echo "==============> CLONING SDK =============="
     echo
 
     cd $SOURCE_PATH
-    git clone git://github.com/alexa/avs-device-sdk.git
+    git clone -b xmos_v1.6 git://github.com/xmos/avs-device-sdk.git
 
     # make the SDK
     echo
@@ -204,7 +246,9 @@ then
 
     cd $BUILD_PATH
     cmake "$SOURCE_PATH/avs-device-sdk" \
-    "${CMAKE_PLATFORM_SPECIFIC[@]}" \
+    -DSENSORY_KEY_WORD_DETECTOR=ON \
+    -DSENSORY_KEY_WORD_DETECTOR_LIB_PATH=$THIRD_PARTY_PATH/alexa-rpi/lib/libsnsr.a \
+    -DSENSORY_KEY_WORD_DETECTOR_INCLUDE_DIR=$THIRD_PARTY_PATH/alexa-rpi/include \
     -DGSTREAMER_MEDIA_PLAYER=ON -DPORTAUDIO=ON \
     -DPORTAUDIO_LIB_PATH="$THIRD_PARTY_PATH/portaudio/lib/.libs/libportaudio.$LIB_SUFFIX" \
     -DPORTAUDIO_INCLUDE_DIR="$THIRD_PARTY_PATH/portaudio/include" \
@@ -245,6 +289,11 @@ cat << EOF > "$CONFIG_FILE"
         "deviceSerialNumber":"$DEVICE_SERIAL_NUMBER",
         "refreshToken":"",
         "productId":"$PRODUCT_ID"
+    },
+    "sampleApp":{
+        "portAudio":{
+            "suggestedLatency": 0.150
+        }
     }
 }
 EOF
@@ -255,23 +304,42 @@ echo
 cat $CONFIG_FILE
 
 cat << EOF > "$START_AUTH_SCRIPT"
-cd "$BUILD_PATH"
-python AuthServer/AuthServer.py
+pushd $BUILD_PATH > /dev/null
+sudo fuser -k -TERM -n tcp 3000
+python AuthServer/AuthServer.py | grep -v '400' &
+chromium-browser http://localhost:3000 > /dev/null 2>&1
+wait
+popd > /dev/null
 EOF
+chmod +x "$START_AUTH_SCRIPT"
 
-generate_start_script
-
-cat << EOF > "$TEST_SCRIPT" 
+cat << EOF > "$TEST_SCRIPT"
 echo
 echo "==============> BUILDING Tests =============="
 echo
 mkdir -p "$UNIT_TEST_MODEL_PATH"
 cp "$UNIT_TEST_MODEL" "$UNIT_TEST_MODEL_PATH"
-cd $BUILD_PATH
+pushd $BUILD_PATH > /dev/null
 make all test -j2
-chmod +x "$START_SCRIPT"
-chmod +x "$START_AUTH_SCRIPT"
+popd > /dev/null
+EOF
+chmod +x "$TEST_SCRIPT"
+
+cat << EOF > "$ALIASES"
+alias avsrun="$BUILD_PATH/SampleApp/src/SampleApp $CONFIG_FILE $THIRD_PARTY_PATH/alexa-rpi/models"
+alias avsunit="$TEST_SCRIPT"
+alias avssetup="$THIS_SCRIPT"
+alias avsauth="$START_AUTH_SCRIPT"
+echo "Available AVS aliases:"
+echo -e "\tavsrun, avsunit, avssetup, avsauth"
+echo "If authentication fails, please check $BUILD_PATH/Integration/AlexaClientSDKConfig.json"
+echo "Remove .bash_aliases and open a new terminal to remove bindings"
 EOF
 
-echo " **** Completed Configuration/Build ***"
+echo
+echo "==============> AUTHENTICATION =============="
+echo
 
+$START_AUTH_SCRIPT
+
+echo " **** Completed Configuration/Build ***"
