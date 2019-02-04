ee1558ddfaa9f00315ed684ca7ce76090b7c3652
oscarbailey-xmos <40995889+oscarbailey-xmos@users.noreply.github.com>
2019-02-04 17:46:08 +0000

    Fix autostart issue

--- tools/Install/setup.sh
+++ tools/Install/setup.sh
@@ -180,8 +180,11 @@ chmod a+rx $AUTH_DETAILS
 # Create autostart script
 AUTOSTART_SESSION="avsrun"
 AUTOSTART_DIR=$HOME/.config/lxsession/LXDE-pi
-mkdir -p $AUTOSTART_DIR
 AUTOSTART=$AUTOSTART_DIR/autostart
+if [ ! -f $AUTOSTART ]; then
+    mkdir -p $AUTOSTART_DIR
+    cp /etc/xdg/lxsession/LXDE-pi/autostart $AUTOSTART
+fi
 STARTUP_SCRIPT=$CURRENT_DIR/.avsrun-startup.sh
 cat << EOF > "$STARTUP_SCRIPT"
 #!/bin/bash
