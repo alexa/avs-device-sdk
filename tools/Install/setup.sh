8114b082414956dbe9ed86fb3d85b2686371822d
Luciano Martin <lucianom@xmos.com>
2019-07-19 13:33:13 +0100

    Port xmos_v1.6 changes

--- tools/Install/setup.sh
+++ tools/Install/setup.sh
@@ -15,10 +15,15 @@
 # permissions and limitations under the License.
 #
 
+#
+# Modified by XMOS Ltd
+# https://github.com/xmos/avs-device-sdk
+#
+
 set -o errexit  # Exit the script if any statement fails.
 set -o nounset  # Exit the script if any uninitialized variable is used.
 
-CLONE_URL=${CLONE_URL:- 'git://github.com/alexa/avs-device-sdk.git'}
+CLONE_URL=${CLONE_URL:- 'git://github.com/xmos/avs-device-sdk.git'}
 
 PORT_AUDIO_FILE="pa_stable_v190600_20161030.tgz"
 PORT_AUDIO_DOWNLOAD_URL="http://www.portaudio.com/archives/$PORT_AUDIO_FILE"
@@ -53,6 +58,12 @@ TEST_SCRIPT="$INSTALL_BASE/test.sh"
 LIB_SUFFIX="a"
 ANDROID_CONFIG_FILE=""
 
+PI_HAT_CTRL_PATH="$THIRD_PARTY_PATH/pi_hat_ctrl"
+ALIASES="$HOME/.bash_aliases"
+
+# Default value for XMOS device
+XMOS_DEVICE="xvf3510"
+
 # Default device serial number if nothing is specified
 DEVICE_SERIAL_NUMBER="123456"
 
@@ -74,12 +85,6 @@ build_port_audio() {
   popd
 }
 
-PI_HAT_CTRL_PATH="$THIRD_PARTY_PATH/pi_hat_ctrl"
-
-SENSORY_MODEL_HASH=5d811d92fb89043f4a4a7b7d0d26d7c3c83899b0
-ALIASES=$HOME/.bash_aliases
-
-
 get_platform() {
   uname_str=`uname -a`
   result=""
@@ -98,18 +103,20 @@ get_platform() {
 }
 
 show_help() {
-  echo  'Usage: setup.sh <config-json-file> [OPTIONS]'
+  echo  'Usage: setup.sh <config-json-file> <xmos-tag> [OPTIONS]'
   echo  'The <config-json-file> can be downloaded from developer portal and must contain the following:'
   echo  '   "clientId": "<OAuth client ID>"'
   echo  '   "productId": "<your product name for device>"'
+  echo  ' The  <xmos-tag> is the tag in the GIT repository xmos/avs-device-sdk'
   echo  ''
   echo  'Optional parameters'
-  echo  '  -s <serial-number>  If nothing is provided, the default device serial number is 123456'
-  echo  '  -a <file-name>      The file that contains Android installation configurations (e.g. androidConfig.txt)'
-  echo  '  -h                  Display this help and exit'
+  echo  '  -s <serial-number>    If nothing is provided, the default device serial number is 123456'
+  echo  '  -a <file-name>        The file that contains Android installation configurations (e.g. androidConfig.txt)'
+  echo  '  -d <xmos-device-type> XMOS device to setup: default xvf3510, possible value xvf3500'
+  echo  '  -h                    Display this help and exit'
 }
 
-if [[ $# -lt 1 ]]; then
+if [[ $# -lt 2 ]]; then
     show_help
     exit 1
 fi
@@ -120,9 +127,12 @@ if [ ! -f "$CONFIG_JSON_FILE" ]; then
     show_help
     exit 1
 fi
+
+XMOS_TAG=$2
+
 shift 1
 
-OPTIONS=s:a:h
+OPTIONS=s:a:d:h
 while getopts "$OPTIONS" opt ; do
     case $opt in
         s )
@@ -135,6 +145,9 @@ while getopts "$OPTIONS" opt ; do
                 exit 1
             fi
             source $ANDROID_CONFIG_FILE
+            ;;
+        d )
+            XMOS_DEVICE="$OPTARG"
             ;;
         h )
             show_help
@@ -209,78 +222,10 @@ else
   exit 1
 fi
 
-AUTH_DETAILS=$CURRENT_DIR/.auth_details
-if [[ -e $AUTH_DETAILS ]]; then
-    echo "==============> PREVIOUS AUTH DETAILS ============"
-    echo
-    cat $AUTH_DETAILS
-    while true; do
-        read -p "Do you wish to use the auth details listed above (y/n)? " ANSWER
-        case ${ANSWER} in
-            y|Y|yes|YES )
-                source $AUTH_DETAILS
-                break;;
-            n|N|no|NO )
-                break;;
-        esac
-    done
-fi
 
-config_userinput(){
-    while [[ -z $CLIENT_ID ]] ; do
-        echo "Enter your Client ID:"
-        read CLIENT_ID
-        if [[ ! "$CLIENT_ID" =~ amzn1\.application-oa2-client\.[0-9a-z]{32} ]]
-        then
-           echo 'Client ID is invalid!'
-           unset CLIENT_ID
-        fi
-    done
-    while [[ -z $CLIENT_SECRET ]] ; do
-        echo "Enter your Client Secret:"
-        read CLIENT_SECRET
-        if [[ ! "$CLIENT_SECRET" =~ [0-9a-f]{64} ]]
-        then
-           echo 'Client Secret is invalid!'
-           unset CLIENT_SECRET
-        fi
-    done
-    while [[ -z $PRODUCT_ID ]] ; do
-        echo "Enter your Product ID:"
-        read PRODUCT_ID
-        if [[ ! "$PRODUCT_ID" =~ [0-9a-zA-Z_]+ ]]
-        then
-           echo 'Product ID is invalid!'
-           unset PRODUCT_ID
-        fi
-    done
-    while [[ -z $DEVICE_SERIAL_NUMBER ]] ; do
-        echo "Enter your Device Serial Number (this can be any number):"
-        read DEVICE_SERIAL_NUMBER
-        if [[ ! "$DEVICE_SERIAL_NUMBER" =~ [0-9a-zA-Z_]+ ]]
-        then
-           echo 'device serial number is invalid!'
-           unset DEVICE_SERIAL_NUMBER
-        fi
-    done
-    while [[ ( "$LOCALE" != "en-US" ) && \
-             ( "$LOCALE" != "en-GB" ) && \
-             ( "$LOCALE" != "de-DE" ) ]] ; do
-        echo "Enter your desired locale (valid values are en-US, en-GB, de-DE):"
-        read LOCALE
-    done
-}
-config_userinput
-
-# Store auth details
-cat << EOF > "$AUTH_DETAILS"
-CLIENT_ID="$CLIENT_ID"
-CLIENT_SECRET="$CLIENT_SECRET"
-PRODUCT_ID="$PRODUCT_ID"
-DEVICE_SERIAL_NUMBER="$DEVICE_SERIAL_NUMBER"
-LOCALE="$LOCALE"
-EOF
-chmod a+rx $AUTH_DETAILS
+echo
+echo "==============> CREATING AUTOSTART SCRIPT ============"
+echo
 
 
 # Create autostart script
@@ -294,7 +239,7 @@ fi
 STARTUP_SCRIPT=$CURRENT_DIR/.avsrun-startup.sh
 cat << EOF > "$STARTUP_SCRIPT"
 #!/bin/bash
-$BUILD_PATH/SampleApp/src/SampleApp $CONFIG_FILE $THIRD_PARTY_PATH/alexa-rpi/models
+$BUILD_PATH/SampleApp/src/SampleApp $OUTPUT_CONFIG_FILE $THIRD_PARTY_PATH/alexa-rpi/models
 \$SHELL
 EOF
 chmod a+rx $STARTUP_SCRIPT
@@ -302,16 +247,14 @@ while true; do
     read -p "Automatically run AVS SDK at startup (y/n)? " ANSWER
     case ${ANSWER} in
         n|N|no|NO )
-            grep $AUTOSTART_SESSION $AUTOSTART > /dev/null 2>&1
-            if [ $? == 0 ]; then
+            if grep $AUTOSTART_SESSION $AUTOSTART; then
                 # Remove startup script from autostart file
                 sed -i '/'"$AUTOSTART_SESSION"'/d' $AUTOSTART
             fi
             break;;
         y|Y|yes|YES )
-            grep $AUTOSTART_SESSION $AUTOSTART > /dev/null 2>&1
-            if [ $? != 0 ]; then #avsrun not present
-                if ! grep "vocalfusion_3510_sales_demo" $AUTOSTART ; then #vocalfusion_3510_sales_demo not present
+            if ! grep $AUTOSTART_SESSION $AUTOSTART; then #avsrun not present
+                if ! grep "vocalfusion_3510_sales_demo" $AUTOSTART; then #vocalfusion_3510_sales_demo not present
                     # Append startup script if not already in autostart file
                     echo "@lxterminal -t $AUTOSTART_SESSION --geometry=150x50 -e $STARTUP_SCRIPT" >> $AUTOSTART
                 fi
@@ -326,18 +269,16 @@ while true; do
     esac
 done
 
-set -e
 
 if [ ! -d "$BUILD_PATH" ]
 then
 
   # Make sure required packages are installed
+  echo
   echo "==============> INSTALLING REQUIRED TOOLS AND PACKAGE ============"
   echo
 
-    sudo apt-get update
-    sudo apt-get -y install git gcc cmake build-essential libsqlite3-dev libcurl4-openssl-dev libfaad-dev libsoup2.4-dev libgcrypt20-dev libgstreamer-plugins-bad1.0-dev gstreamer1.0-plugins-good libasound2-dev sox gedit vim python3-pip
-    pip install flask commentjson
+  install_dependencies
 
   # create / paths
   echo
@@ -351,6 +292,13 @@ then
 
   run_os_specifics
 
+  if [ $XMOS_DEVICE = "xvf3510" ]
+  then
+    PI_HAT_FLAG="-DPI_HAT_CTRL=ON"
+  else
+    PI_HAT_FLAG=""
+  fi
+
   if [ ! -d "${SOURCE_PATH}/avs-device-sdk" ]
   then
     #get sdk
@@ -359,46 +307,31 @@ then
     echo
 
     cd $SOURCE_PATH
-    git clone -b xmos_v1.6 git://github.com/xmos/avs-device-sdk.git
+    git clone -b $XMOS_TAG $CLONE_URL
+    if [ $XMOS_DEVICE = "xvf3510" ]
+    then
+      echo
+      echo "==============> BUILDING PI HAT CONTROL =============="
+      echo
 
-
-    if [ $# -ge 1 ] && [ $1 = "xvf3510" ] ; then
-        echo
-        echo "==============> BUILDING PI HAT CONTROL =============="
-        echo
-
-        mkdir -p $PI_HAT_CTRL_PATH
-        pushd $SOURCE_PATH/avs-device-sdk/ThirdParty/pi_hat_ctrl > /dev/null
-        gcc pi_hat_ctrl.c -o $PI_HAT_CTRL_PATH/pi_hat_ctrl -lwiringPi -lm
-        popd > /dev/null
+      mkdir -p $PI_HAT_CTRL_PATH
+      pushd $SOURCE_PATH/avs-device-sdk/ThirdParty/pi_hat_ctrl > /dev/null
+      gcc pi_hat_ctrl.c -o $PI_HAT_CTRL_PATH/pi_hat_ctrl -lm
+      popd > /dev/null
     fi
+  fi
 
   # make the SDK
   echo
   echo "==============> BUILDING SDK =============="
   echo
 
-    cd $BUILD_PATH
-    if [ $# -ge 1 ] && [ $1 = "xvf3510" ] ; then
-        cmake "$SOURCE_PATH/avs-device-sdk" \
-        -DSENSORY_KEY_WORD_DETECTOR=ON \
-        -DSENSORY_KEY_WORD_DETECTOR_LIB_PATH=$THIRD_PARTY_PATH/alexa-rpi/lib/libsnsr.a \
-        -DSENSORY_KEY_WORD_DETECTOR_INCLUDE_DIR=$THIRD_PARTY_PATH/alexa-rpi/include \
-        -DGSTREAMER_MEDIA_PLAYER=ON -DPORTAUDIO=ON \
-        -DPORTAUDIO_LIB_PATH="$THIRD_PARTY_PATH/portaudio/lib/.libs/libportaudio.$LIB_SUFFIX" \
-        -DPORTAUDIO_INCLUDE_DIR="$THIRD_PARTY_PATH/portaudio/include" \
-        -DCMAKE_BUILD_TYPE=DEBUG \
-        -DPI_HAT_CTRL=ON
-    else
-        cmake "$SOURCE_PATH/avs-device-sdk" \
-        -DSENSORY_KEY_WORD_DETECTOR=ON \
-        -DSENSORY_KEY_WORD_DETECTOR_LIB_PATH=$THIRD_PARTY_PATH/alexa-rpi/lib/libsnsr.a \
-        -DSENSORY_KEY_WORD_DETECTOR_INCLUDE_DIR=$THIRD_PARTY_PATH/alexa-rpi/include \
-        -DGSTREAMER_MEDIA_PLAYER=ON -DPORTAUDIO=ON \
-        -DPORTAUDIO_LIB_PATH="$THIRD_PARTY_PATH/portaudio/lib/.libs/libportaudio.$LIB_SUFFIX" \
-        -DPORTAUDIO_INCLUDE_DIR="$THIRD_PARTY_PATH/portaudio/include" \
-        -DCMAKE_BUILD_TYPE=DEBUG
-    fi
+  mkdir -p $BUILD_PATH
+  cd $BUILD_PATH
+  cmake "$SOURCE_PATH/avs-device-sdk" \
+      $PI_HAT_FLAG \
+      -DCMAKE_BUILD_TYPE=DEBUG \
+      "${CMAKE_PLATFORM_SPECIFIC[@]}"
 
   cd $BUILD_PATH
   make SampleApp -j2
@@ -420,7 +353,7 @@ cat << EOF > "$OUTPUT_CONFIG_FILE"
     },
 EOF
 
-cd $INSTALL_BASE
+cd $CURRENT_DIR
 bash genConfig.sh config.json $DEVICE_SERIAL_NUMBER $CONFIG_DB_PATH $SOURCE_PATH/avs-device-sdk $TEMP_CONFIG_FILE
 
 # Delete first line from temp file to remove opening bracket
@@ -429,35 +362,23 @@ sed -i -e "1d" $TEMP_CONFIG_FILE
 # Append temp file to configuration file
 cat $TEMP_CONFIG_FILE >> $OUTPUT_CONFIG_FILE
 
+# Enable the suggestedLatency parameter for portAudio
+sed -i -e '/displayCardsSupported/s/$/,/' $OUTPUT_CONFIG_FILE
+sed -i -e '/portAudio/s/\/\///' $OUTPUT_CONFIG_FILE
+# the sed command below will remove the // on two consecutive lines
+sed -i -e '/suggestedLatency/{N;s/\/\///g;}' $OUTPUT_CONFIG_FILE
+
 # Delete temp file
 rm $TEMP_CONFIG_FILE
 
 echo
-echo "==============> FINAL CONFIGURATION  =============="
+echo "==============> FINAL CONFIGURATION AND ALIASES =============="
 echo
-cat $CONFIG_FILE
+cat $OUTPUT_CONFIG_FILE
 
-cat << EOF > "$START_AUTH_SCRIPT"
-pushd $BUILD_PATH > /dev/null
-sudo fuser -k -TERM -n tcp 3000
-python AuthServer/AuthServer.py | grep -v '400' &
-chromium-browser http://localhost:3000 > /dev/null 2>&1
-wait
-popd > /dev/null
-EOF
-chmod +x "$START_AUTH_SCRIPT"
+generate_start_script
 
-cat << EOF > "$TEST_SCRIPT"
-echo
-echo "==============> BUILDING Tests =============="
-echo
-mkdir -p "$UNIT_TEST_MODEL_PATH"
-cp "$UNIT_TEST_MODEL" "$UNIT_TEST_MODEL_PATH"
-pushd $BUILD_PATH > /dev/null
-make all test -j2
-popd > /dev/null
-EOF
-chmod +x "$TEST_SCRIPT"
+generate_test_script
 
 if [ ! -f $ALIASES ] ; then
 echo "Create .bash_aliases file"
@@ -473,20 +394,13 @@ sed -i '/AVS/d' $ALIASES > /dev/null
 sed -i '/AlexaClientSDKConfig.json/d' $ALIASES > /dev/null
 sed -i '/Remove/d' $ALIASES > /dev/null
 
-echo "alias avsrun="$BUILD_PATH/SampleApp/src/SampleApp $CONFIG_FILE $THIRD_PARTY_PATH/alexa-rpi/models"" >> $ALIASES
-echo "alias avsunit="$TEST_SCRIPT"" >> $ALIASES
-echo "alias avssetup="$THIS_SCRIPT"" >> $ALIASES
-echo "alias avsauth="$START_AUTH_SCRIPT"" >> $ALIASES
+echo "alias avsrun=\"$BUILD_PATH/SampleApp/src/SampleApp $OUTPUT_CONFIG_FILE $THIRD_PARTY_PATH/alexa-rpi/models\"" >> $ALIASES
+echo "alias avsunit=\"bash $TEST_SCRIPT\"" >> $ALIASES
+echo "alias avssetup=\"cd $CURRENT_DIR; bash setup.sh\"" >> $ALIASES
 echo "echo "Available AVS aliases:"" >> $ALIASES
 echo "echo -e "avsrun, avsunit, avssetup, avsauth"" >> $ALIASES
 echo "echo "If authentication fails, please check $BUILD_PATH/Integration/AlexaClientSDKConfig.json"" >> $ALIASES
 echo "echo "Remove .bash_aliases and open a new terminal to remove bindings"" >> $ALIASES
 
-
-echo
-echo "==============> AUTHENTICATION =============="
-echo
-
-$START_AUTH_SCRIPT
-
 echo " **** Completed Configuration/Build ***"
+
