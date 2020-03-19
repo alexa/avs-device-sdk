2be62331ca75da01d388063a137987c7de064241
tjyuanxi <34560457+tjyuanxi@users.noreply.github.com>
2020-03-19 14:20:11 +0800

    Update Timer.h
    There is a bug in the Timer. IF maney threads want to stop a timer, it maybe joinable, and then they will join at one time ,so it will crash.
    There lack a lock.

--- AVSCommon/Utils/include/AVSCommon/Utils/Timing/Timer.h
+++ AVSCommon/Utils/include/AVSCommon/Utils/Timing/Timer.h
@@ -217,7 +217,7 @@ private:
 
     /// The mutex for @c m_waitCondition.
     std::mutex m_waitMutex;
-
+    std::mutex m_joinMutex;
     /// The thread to execute tasks on.
     std::thread m_thread;
 
