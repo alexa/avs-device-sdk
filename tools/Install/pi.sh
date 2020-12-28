d370bc0acab01429e3b8dfb794f089778afd254f
Anders Aaen Springborg <aaspringborg@gmail.com>
2020-12-28 22:39:47 +0100

    Update pi.sh

--- tools/Install/pi.sh
+++ tools/Install/pi.sh
@@ -35,7 +35,7 @@ GSTREAMER_AUDIO_SINK="alsasink"
 install_dependencies() {
   sudo apt-get update
   sudo apt-get -y install git gcc cmake build-essential libsqlite3-dev libssl-dev libnghttp2-dev libfaad-dev libsoup2.4-dev libgcrypt20-dev libgstreamer-plugins-bad1.0-dev gstreamer1.0-plugins-good libasound2-dev sox gedit vim python3-pip
-  pip install flask commentjson
+  pip3 install flask commentjson
 }
 
 run_os_specifics() {
