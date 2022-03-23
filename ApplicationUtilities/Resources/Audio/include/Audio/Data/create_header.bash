7f9b5b7885640cf0df676ef3a777d456332d284b
Lucas Frossard <lffrossa@amazon.com>
2022-03-23 23:39:46 +0000

    Correct accidental addition of Apache license text at top of some earcon files.

--- ApplicationUtilities/Resources/Audio/include/Audio/Data/create_header.bash
+++ ApplicationUtilities/Resources/Audio/include/Audio/Data/create_header.bash
@@ -17,21 +17,6 @@ CURR_YEAR=`date +"%Y"`
 
 cat <<EOF > "${FULL_OUTPUT}"
 /*
- * Copyright ${CURR_YEAR} Amazon.com, Inc. or its affiliates. All Rights Reserved.
- *
- * Licensed under the Apache License, Version 2.0 (the "License").
- * You may not use this file except in compliance with the License.
- * A copy of the License is located at
- *
- *     http://aws.amazon.com/apache2.0/
- *
- * or in the "license" file accompanying this file. This file is distributed
- * on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
- * express or implied. See the License for the specific language governing
- * permissions and limitations under the License.
- */
-
-/*
  * ******************
  * ALEXA AUDIO ASSETS
  * ******************
@@ -39,9 +24,9 @@ cat <<EOF > "${FULL_OUTPUT}"
  * Copyright ${CURR_YEAR} Amazon.com, Inc. or its affiliates ("Amazon").
  * All Rights Reserved.
  *
- * These materials are licensed to you as "Alexa Materials" under the Alexa Voice
- * Service Agreement, which is currently available at
- * https://developer.amazon.com/public/solutions/alexa/alexa-voice-service/support/terms-and-agreements.
+ * These materials are licensed to you as “AVS Materials" under the Amazon
+ * Developer Services Agreement, which is currently available at
+ * https://developer.amazon.com/support/legal/da
  */
 EOF
 
--- ApplicationUtilities/Resources/Audio/include/Audio/Data/med_ui_wakesound.wav.h
+++ ApplicationUtilities/Resources/Audio/include/Audio/Data/med_ui_wakesound.wav.h
@@ -1,29 +1,14 @@
 /*
- * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
- *
- * Licensed under the Apache License, Version 2.0 (the "License").
- * You may not use this file except in compliance with the License.
- * A copy of the License is located at
- *
- *     http://aws.amazon.com/apache2.0/
- *
- * or in the "license" file accompanying this file. This file is distributed
- * on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
- * express or implied. See the License for the specific language governing
- * permissions and limitations under the License.
- */
-
-/*
  * ******************
  * ALEXA AUDIO ASSETS
  * ******************
  *
- * Copyright 2019-2020 Amazon.com, Inc. or its affiliates ("Amazon").
+ * Copyright 2019-2022 Amazon.com, Inc. or its affiliates ("Amazon").
  * All Rights Reserved.
  *
- * These materials are licensed to you as "Alexa Materials" under the Alexa Voice
- * Service Agreement, which is currently available at
- * https://developer.amazon.com/public/solutions/alexa/alexa-voice-service/support/terms-and-agreements.
+ * These materials are licensed to you as “AVS Materials" under the Amazon
+ * Developer Services Agreement, which is currently available at
+ * https://developer.amazon.com/support/legal/da
  */
 
 #ifndef ALEXA_CLIENT_SDK_APPLICATIONUTILITIES_RESOURCES_AUDIO_INCLUDE_AUDIO_DATA_MED_UI_WAKESOUND_WAV_H_
